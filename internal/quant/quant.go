add98276fc0871412268d17216448c3f276c6731
Pettecco <petterson.ikaro@gec.inatel.br>
2026-05-11 21:14:47 -0300

    feat: add int16 quantization for vector distance computation
    - Scale factor 10000 with math.Round (half-away-from-zero)
    - Squared L2 distance in int64 to prevent overflow
    - Tests for encoding and distance order preservation

---
+++ internal/quant/quant.go
@@ -0,0 +1,34 @@
+package quant
+
+import (
+	"math"
+	"rinha26/internal/consts"
+)
+
+func EncodeFloat(v float64) int16 {
+	return int16(math.Round(v * consts.Scale))
+}
+
+func EncodeVec(src [consts.Dim]float64, dst *[consts.Dim]int16) {
+	for i := 0; i < consts.Dim; i++ {
+		dst[i] = EncodeFloat(src[i])
+	}
+}
+
+func DistSqRaw(a, b [consts.Dim]int16) int64 {
+	var sum int64
+	for i := 0; i < consts.Dim; i++ {
+		d := int64(a[i]) - int64(b[i])
+		sum += d * d
+	}
+	return sum
+}
+
+func DistSqF64(a, b [consts.Dim]float64) float64 {
+	var sum float64
+	for i := 0; i < consts.Dim; i++ {
+		d := a[i] - b[i]
+		sum += d * d
+	}
+	return sum
+}
