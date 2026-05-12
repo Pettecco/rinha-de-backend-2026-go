d85602bac7ec3c566e92db544c3a5c2faa9ed78e
Pettecco <petterson.ikaro@gec.inatel.br>
2026-05-12 20:16:25 -0300

    perf: refactor ivf search to use quantize package and unroll loops
    - Use quantize.EncodeFloat for query quantization
    - Unroll computeCentroidDistances dot product loop
    - Unroll scanClusters distance computation loop
    - Change queryI16 from int32 to int16

--- internal/ivf/search.go
+++ internal/ivf/search.go
@@ -1,17 +1,17 @@
 package ivf
 
 import (
-	"math"
 	"sort"
 	"sync"
 
 	"rinha26/internal/consts"
+	"rinha26/internal/quantize"
 )
 
 func (idx *Index) FraudScore(query [consts.Dim]float64, nProbeFast, nProbeFull int) int {
-	var queryI16 [consts.Dim]int32
+	var queryI16 [consts.Dim]int16
 	for d := 0; d < consts.Dim; d++ {
-		queryI16[d] = int32(math.Round(query[d] * consts.Scale))
+		queryI16[d] = quantize.EncodeFloat(query[d])
 	}
 
 	dists := centroidDistPool.Get().([]float64)
@@ -40,16 +40,26 @@ func (idx *Index) computeCentroidDistances(query [consts.Dim]float64, dst []floa
 	norms := idx.CentroidNormsData()
 	centroids := idx.CentroidsF64Data()
 	for c := 0; c < k; c++ {
-		var dot float64
 		base := c * consts.Dim
-		for d := 0; d < consts.Dim; d++ {
-			dot += centroids[base+d] * query[d]
-		}
+		dot := centroids[base]*query[0] +
+			centroids[base+1]*query[1] +
+			centroids[base+2]*query[2] +
+			centroids[base+3]*query[3] +
+			centroids[base+4]*query[4] +
+			centroids[base+5]*query[5] +
+			centroids[base+6]*query[6] +
+			centroids[base+7]*query[7] +
+			centroids[base+8]*query[8] +
+			centroids[base+9]*query[9] +
+			centroids[base+10]*query[10] +
+			centroids[base+11]*query[11] +
+			centroids[base+12]*query[12] +
+			centroids[base+13]*query[13]
 		dst[c] = norms[c] - 2*dot
 	}
 }
 
-func (idx *Index) scanClusters(clusters []int, queryI16 [consts.Dim]int32, topK *TopK) {
+func (idx *Index) scanClusters(clusters []int, queryI16 [consts.Dim]int16, topK *TopK) {
 	offsets := idx.OffsetsData()
 	blocks := idx.BlocksData()
 	labels := idx.LabelsData()
@@ -62,14 +72,24 @@ func (idx *Index) scanClusters(clusters []int, queryI16 [consts.Dim]int32, topK 
 			labelStart := int(b) * consts.BlockSize
 
 			for v := 0; v < consts.BlockSize; v++ {
-				var dist int64
-				for d := 0; d < consts.Dim; d++ {
-					val := int64(blocks[blockStart+d*consts.BlockSize+v])
-					diff := int64(queryI16[d]) - val
-					dist += diff * diff
-				}
-				isFraud := labels[labelStart+v] == 1
-				topK.Push(uint32(dist), isFraud)
+				d0 := int64(queryI16[0]) - int64(blocks[blockStart+0*consts.BlockSize+v])
+				d1 := int64(queryI16[1]) - int64(blocks[blockStart+1*consts.BlockSize+v])
+				d2 := int64(queryI16[2]) - int64(blocks[blockStart+2*consts.BlockSize+v])
+				d3 := int64(queryI16[3]) - int64(blocks[blockStart+3*consts.BlockSize+v])
+				d4 := int64(queryI16[4]) - int64(blocks[blockStart+4*consts.BlockSize+v])
+				d5 := int64(queryI16[5]) - int64(blocks[blockStart+5*consts.BlockSize+v])
+				d6 := int64(queryI16[6]) - int64(blocks[blockStart+6*consts.BlockSize+v])
+				d7 := int64(queryI16[7]) - int64(blocks[blockStart+7*consts.BlockSize+v])
+				d8 := int64(queryI16[8]) - int64(blocks[blockStart+8*consts.BlockSize+v])
+				d9 := int64(queryI16[9]) - int64(blocks[blockStart+9*consts.BlockSize+v])
+				d10 := int64(queryI16[10]) - int64(blocks[blockStart+10*consts.BlockSize+v])
+				d11 := int64(queryI16[11]) - int64(blocks[blockStart+11*consts.BlockSize+v])
+				d12 := int64(queryI16[12]) - int64(blocks[blockStart+12*consts.BlockSize+v])
+				d13 := int64(queryI16[13]) - int64(blocks[blockStart+13*consts.BlockSize+v])
+				dist := d0*d0 + d1*d1 + d2*d2 + d3*d3 + d4*d4 + d5*d5 + d6*d6 +
+					d7*d7 + d8*d8 + d9*d9 + d10*d10 + d11*d11 + d12*d12 + d13*d13
+
+				topK.Push(uint32(dist), labels[labelStart+v] == 1)
 			}
 		}
 	}
--- internal/vector/config.go
+++ internal/vector/config.go
@@ -17,21 +17,6 @@ func LoadNorm(path string) (*Norm, error) {
 	}
 	return norm, nil
 }
-	norm := &Norm{}
-	if err := json.Unmarshal(raw, norm); err != nil {
-		return nil, err
-	}
-	if norm.MaxAmount == 0 ||
-		norm.MaxInstallments == 0 ||
-		norm.AmountVsAvgRatio == 0 ||
-		norm.MaxMinutes == 0 ||
-		norm.MaxKm == 0 ||
-		norm.MaxTxCount24h == 0 ||
-		norm.MaxMerchantAvgAmount == 0 {
-		return nil, errors.New("normalization.json has zero constants")
-	}
-	return norm, nil
-}
 
 // LoadMccRisk reads mcc_risk.json into a MccRisk map.
 func LoadMccRisk(path string) (MccRisk, error) {
